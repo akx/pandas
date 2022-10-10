akx/pandas

commit 6d1541e1782a7b94797d5432922e64a97934cfa4

REF: move get_filepath_buffer into get_handle (#37639)

pandas/io/stata.py
@@ -53,12 +53,7 @@ from pandas.core.frame import DataFrame
 from pandas.core.indexes.base import Index
 from pandas.core.series import Series
 
-from pandas.io.common import (
-    IOHandles,
-    get_filepath_or_buffer,
-    get_handle,
-    stringify_path,
-)
+from pandas.io.common import get_handle
 
 _version_error = (
     "Version of given Stata file is {version}. pandas supports importing "
@@ -1062,20 +1057,15 @@ class StataReader(StataParser, abc.Iterator):
         self._lines_read = 0
 
         self._native_byteorder = _set_endianness(sys.byteorder)
-        self.ioargs = get_filepath_or_buffer(
-            path_or_buf, storage_options=storage_options
-        )
-
-        if isinstance(self.ioargs.filepath_or_buffer, (str, bytes)):
-            self.ioargs.filepath_or_buffer = open(self.ioargs.filepath_or_buffer, "rb")
-            self.ioargs.should_close = True
-        elif hasattr(path_or_buf, "read"):
+        with get_handle(
+            path_or_buf,
+            "rb",
+            storage_options=storage_options,
+            is_text=False,
+        ) as handles:
             # Copy to BytesIO, and ensure no encoding
-            contents = self.ioargs.filepath_or_buffer.read()
-            self.ioargs.close()
-            self.ioargs.filepath_or_buffer = BytesIO(contents)  # type: ignore[arg-type]
-            self.ioargs.should_close = True
-        self.path_or_buf = cast(BytesIO, self.ioargs.filepath_or_buffer)
+            contents = handles.handle.read()
+        self.path_or_buf = BytesIO(contents)  # type: ignore[arg-type]
 
         self._read_header()
         self._setup_dtype()
@@ -1090,7 +1080,7 @@ class StataReader(StataParser, abc.Iterator):
 
     def close(self) -> None:
         """ close the handle if its open """
-        self.ioargs.close()
+        self.path_or_buf.close()
 
     def _set_encoding(self) -> None:
         """
@@ -1932,48 +1922,6 @@ def read_stata(
     return data
 
 
-def _open_file_binary_write(
-    fname: FilePathOrBuffer,
-    compression: CompressionOptions,
-    storage_options: StorageOptions = None,
-) -> Tuple[IOHandles, CompressionOptions]:
-    """
-    Open a binary file or no-op if file-like.
-
-    Parameters
-    ----------
-    fname : string path, path object or buffer
-        The file name or buffer.
-    compression : {str, dict, None}
-        The compression method to use.
-
-    storage_options : dict, optional
-        Extra options that make sense for a particular storage connection, e.g.
-        host, port, username, password, etc., if using a URL that will
-        be parsed by ``fsspec``, e.g., starting "s3://", "gcs://". An error
-        will be raised if providing this argument with a local path or
-        a file-like buffer. See the fsspec and backend storage implementation
-        docs for the set of allowed keys and values
-
-        .. versionadded:: 1.2.0
-    """
-    ioargs = get_filepath_or_buffer(
-        fname, mode="wb", compression=compression, storage_options=storage_options
-    )
-    handles = get_handle(
-        ioargs.filepath_or_buffer,
-        "wb",
-        compression=ioargs.compression,
-        is_text=False,
-    )
-    if ioargs.filepath_or_buffer != fname and not isinstance(
-        ioargs.filepath_or_buffer, str
-    ):
-        # add handle created by get_filepath_or_buffer
-        handles.created_handles.append(ioargs.filepath_or_buffer)
-    return handles, ioargs.compression
-
-
 def _set_endianness(endianness: str) -> str:
     if endianness.lower() in ["<", "little"]:
         return "<"
@@ -2231,7 +2179,7 @@ class StataWriter(StataParser):
         if byteorder is None:
             byteorder = sys.byteorder
         self._byteorder = _set_endianness(byteorder)
-        self._fname = stringify_path(fname)
+        self._fname = fname
         self.type_converters = {253: np.int32, 252: np.int16, 251: np.int8}
         self._converted_names: Dict[Label, str] = {}
 
@@ -2511,45 +2459,53 @@ supported types."""
                     self.data[col] = encoded
 
     def write_file(self) -> None:
-        self.handles, compression = _open_file_binary_write(
-            self._fname, self._compression, storage_options=self.storage_options
-        )
-        if compression is not None:
-            # ZipFile creates a file (with the same name) for each write call.
-            # Write it first into a buffer and then write the buffer to the ZipFile.
-            self._output_file = self.handles.handle
-            self.handles.handle = BytesIO()
-        try:
-            self._write_header(data_label=self._data_label, time_stamp=self._time_stamp)
-            self._write_map()
-            self._write_variable_types()
-            self._write_varnames()
-            self._write_sortlist()
-            self._write_formats()
-            self._write_value_label_names()
-            self._write_variable_labels()
-            self._write_expansion_fields()
-            self._write_characteristics()
-            records = self._prepare_data()
-            self._write_data(records)
-            self._write_strls()
-            self._write_value_labels()
-            self._write_file_close_tag()
-            self._write_map()
-        except Exception as exc:
-            self._close()
-            if isinstance(self._fname, (str, Path)):
-                try:
-                    os.unlink(self._fname)
-                except OSError:
-                    warnings.warn(
-                        f"This save was not successful but {self._fname} could not "
-                        "be deleted.  This file is not valid.",
-                        ResourceWarning,
-                    )
-            raise exc
-        else:
-            self._close()
+        with get_handle(
+            self._fname,
+            "wb",
+            compression=self._compression,
+            is_text=False,
+            storage_options=self.storage_options,
+        ) as self.handles:
+
+            if self.handles.compression["method"] is not None:
+                # ZipFile creates a file (with the same name) for each write call.
+                # Write it first into a buffer and then write the buffer to the ZipFile.
+                self._output_file = self.handles.handle
+                self.handles.handle = BytesIO()
+
+            try:
+                self._write_header(
+                    data_label=self._data_label, time_stamp=self._time_stamp
+                )
+                self._write_map()
+                self._write_variable_types()
+                self._write_varnames()
+                self._write_sortlist()
+                self._write_formats()
+                self._write_value_label_names()
+                self._write_variable_labels()
+                self._write_expansion_fields()
+                self._write_characteristics()
+                records = self._prepare_data()
+                self._write_data(records)
+                self._write_strls()
+                self._write_value_labels()
+                self._write_file_close_tag()
+                self._write_map()
+            except Exception as exc:
+                self._close()
+                if isinstance(self._fname, (str, Path)):
+                    try:
+                        os.unlink(self._fname)
+                    except OSError:
+                        warnings.warn(
+                            f"This save was not successful but {self._fname} could not "
+                            "be deleted.  This file is not valid.",
+                            ResourceWarning,
+                        )
+                raise exc
+            else:
+                self._close()
 
     def _close(self) -> None:
         """
@@ -2566,8 +2522,6 @@ supported types."""
             self.handles.handle = self._output_file
             self.handles.handle.write(bio.read())  # type: ignore[arg-type]
             bio.close()
-        # close any created handles
-        self.handles.close()
 
     def _write_map(self) -> None:
         """No-op, future compatibility"""

pandas/_typing.py
@@ -146,10 +146,5 @@ CompressionDict = Dict[str, Any]
 CompressionOptions = Optional[Union[str, CompressionDict]]
 
 
-# let's bind types
-ModeVar = TypeVar("ModeVar", str, None, Optional[str])
-EncodingVar = TypeVar("EncodingVar", str, None, Optional[str])
-
-
 # type of float formatter in DataFrameFormatter
 FloatFormatType = Union[str, Callable, "EngFormatter"]

pandas/core/frame.py
@@ -157,7 +157,7 @@ from pandas.core.reshape.melt import melt
 from pandas.core.series import Series
 from pandas.core.sorting import get_group_index, lexsort_indexer, nargsort
 
-from pandas.io.common import get_filepath_or_buffer
+from pandas.io.common import get_handle
 from pandas.io.formats import console, format as fmt
 from pandas.io.formats.info import DataFrameInfo
 import pandas.plotting
@@ -2301,10 +2301,10 @@ class DataFrame(NDFrame, OpsMixin):
         result = tabulate.tabulate(self, **kwargs)
         if buf is None:
             return result
-        ioargs = get_filepath_or_buffer(buf, mode=mode, storage_options=storage_options)
-        assert not isinstance(ioargs.filepath_or_buffer, (str, mmap.mmap))
-        ioargs.filepath_or_buffer.writelines(result)
-        ioargs.close()
+
+        with get_handle(buf, mode, storage_options=storage_options) as handles:
+            assert not isinstance(handles.handle, (str, mmap.mmap))
+            handles.handle.writelines(result)
         return None
 
     @deprecate_kwarg(old_arg_name="fname", new_arg_name="path")

pandas/core/generic.py
@@ -3221,7 +3221,7 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
             File path or object, if None is provided the result is returned as
             a string.  If a non-binary file object is passed, it should be opened
             with `newline=''`, disabling universal newlines. If a binary
-            file object is passed, `mode` needs to contain a `'b'`.
+            file object is passed, `mode` might need to contain a `'b'`.
 
             .. versionchanged:: 0.24.0
 

pandas/io/common.py
@@ -8,21 +8,7 @@ from io import BufferedIOBase, BytesIO, RawIOBase, TextIOWrapper
 import mmap
 import os
 import pathlib
-from typing import (
-    IO,
-    TYPE_CHECKING,
-    Any,
-    AnyStr,
-    Dict,
-    Generic,
-    List,
-    Mapping,
-    Optional,
-    Tuple,
-    Type,
-    Union,
-    cast,
-)
+from typing import IO, Any, AnyStr, Dict, List, Mapping, Optional, Tuple, cast
 from urllib.parse import (
     urljoin,
     urlparse as parse_url,
@@ -37,10 +23,8 @@ from pandas._typing import (
     Buffer,
     CompressionDict,
     CompressionOptions,
-    EncodingVar,
     FileOrBuffer,
     FilePathOrBuffer,
-    ModeVar,
     StorageOptions,
 )
 from pandas.compat import get_lzma_file, import_lzma
@@ -55,16 +39,10 @@ _VALID_URLS = set(uses_relative + uses_netloc + uses_params)
 _VALID_URLS.discard("")
 
 
-if TYPE_CHECKING:
-    from io import IOBase
-
-
 @dataclasses.dataclass
-class IOArgs(Generic[ModeVar, EncodingVar]):
+class IOArgs:
     """
-    Return value of io/common.py:get_filepath_or_buffer.
-
-    This is used to easily close created fsspec objects.
+    Return value of io/common.py:_get_filepath_or_buffer.
 
     Note (copy&past from io/parsers):
     filepath_or_buffer can be Union[FilePathOrBuffer, s3fs.S3File, gcsfs.GCSFile]
@@ -73,28 +51,18 @@ class IOArgs(Generic[ModeVar, EncodingVar]):
     """
 
     filepath_or_buffer: FileOrBuffer
-    encoding: EncodingVar
-    mode: Union[ModeVar, str]
+    encoding: str
+    mode: str
     compression: CompressionDict
     should_close: bool = False
-
-    def close(self) -> None:
-        """
-        Close the buffer if it was created by get_filepath_or_buffer.
-        """
-        if self.should_close:
-            assert not isinstance(self.filepath_or_buffer, str)
-            try:
-                self.filepath_or_buffer.close()
-            except (OSError, ValueError):
-                pass
-        self.should_close = False
 
 
 @dataclasses.dataclass
 class IOHandles:
     """
     Return value of io/common.py:get_handle
+
+    Can be used as a context manager.
 
     This is used to easily close created buffers and to handle corner cases when
     TextIOWrapper is inserted.
@@ -105,6 +73,7 @@ class IOHandles:
     """
 
     handle: Buffer
+    compression: CompressionDict
     created_handles: List[Buffer] = dataclasses.field(default_factory=list)
     is_wrapped: bool = False
     is_mmap: bool = False
@@ -128,6 +97,12 @@ class IOHandles:
             pass
         self.created_handles = []
         self.is_wrapped = False
+
+    def __enter__(self) -> "IOHandles":
+        return self
+
+    def __exit__(self, *args: Any) -> None:
+        self.close()
 
 
 def is_url(url) -> bool:
@@ -239,18 +214,13 @@ def is_fsspec_url(url: FilePathOrBuffer) -> bool:
     )
 
 
-# https://github.com/python/mypy/issues/8708
-# error: Incompatible default for argument "encoding" (default has type "None",
-# argument has type "str")
-# error: Incompatible default for argument "mode" (default has type "None",
-# argument has type "str")
-def get_filepath_or_buffer(
+def _get_filepath_or_buffer(
     filepath_or_buffer: FilePathOrBuffer,
-    encoding: EncodingVar = None,  # type: ignore[assignment]
+    encoding: str = "utf-8",
     compression: CompressionOptions = None,
-    mode: ModeVar = None,  # type: ignore[assignment]
+    mode: str = "r",
     storage_options: StorageOptions = None,
-) -> IOArgs[ModeVar, EncodingVar]:
+) -> IOArgs:
     """
     If the filepath_or_buffer is a url, translate and return the buffer.
     Otherwise passthrough.
@@ -284,12 +254,7 @@ def get_filepath_or_buffer(
     compression_method = infer_compression(filepath_or_buffer, compression_method)
 
     # GH21227 internal compression is not used for non-binary handles.
-    if (
-        compression_method
-        and hasattr(filepath_or_buffer, "write")
-        and mode
-        and "b" not in mode
-    ):
+    if compression_method and hasattr(filepath_or_buffer, "write") and "b" not in mode:
         warnings.warn(
             "compression has no effect when passing a non-binary object as input.",
             RuntimeWarning,
@@ -306,8 +271,7 @@ def get_filepath_or_buffer(
     # bz2 and xz do not write the byte order mark for utf-16 and utf-32
     # print a warning when writing such files
     if (
-        mode
-        and "w" in mode
+        "w" in mode
         and compression_method in ["bz2", "xz"]
         and encoding in ["utf-16", "utf-32"]
     ):
@@ -319,7 +283,7 @@ def get_filepath_or_buffer(
     # Use binary mode when converting path-like objects to file-like objects (fsspec)
     # except when text mode is explicitly requested. The original mode is returned if
     # fsspec is not used.
-    fsspec_mode = mode or "rb"
+    fsspec_mode = mode
     if "t" not in fsspec_mode and "b" not in fsspec_mode:
         fsspec_mode += "b"
 
@@ -504,12 +468,8 @@ def infer_compression(
     ------
     ValueError on invalid compression specified.
     """
-    # No compression has been explicitly specified
-    if compression is None:
-        return None
-
     # Infer compression
-    if compression == "infer":
+    if compression in ("infer", None):
         # Convert all path types (e.g. pathlib.Path) to strings
         filepath_or_buffer = stringify_path(filepath_or_buffer)
         if not isinstance(filepath_or_buffer, str):
@@ -540,6 +500,7 @@ def get_handle(
     memory_map: bool = False,
     is_text: bool = True,
     errors: Optional[str] = None,
+    storage_options: StorageOptions = None,
 ) -> IOHandles:
     """
     Get file handle for given path/buffer and mode.
@@ -583,66 +544,73 @@ def get_handle(
         Specifies how encoding and decoding errors are to be handled.
         See the errors argument for :func:`open` for a full list
         of options.
+    storage_options: StorageOptions = None
+        Passed to _get_filepath_or_buffer
 
     .. versionchanged:: 1.2.0
 
     Returns the dataclass IOHandles
     """
-    need_text_wrapping: Tuple[Type["IOBase"], ...]
-    try:
-        from s3fs import S3File
-
-        need_text_wrapping = (BufferedIOBase, RawIOBase, S3File)
-    except ImportError:
-        need_text_wrapping = (BufferedIOBase, RawIOBase)
-    # fsspec is an optional dependency. If it is available, add its file-object
-    # class to the list of classes that need text wrapping. If fsspec is too old and is
-    # needed, get_filepath_or_buffer would already have thrown an exception.
-    try:
-        from fsspec.spec import AbstractFileSystem
-
-        need_text_wrapping = (*need_text_wrapping, AbstractFileSystem)
-    except ImportError:
-        pass
-
     # Windows does not default to utf-8. Set to utf-8 for a consistent behavior
     if encoding is None:
         encoding = "utf-8"
 
-    # Convert pathlib.Path/py.path.local or string
-    handle = stringify_path(path_or_buf)
+    # read_csv does not know whether the buffer is opened in binary/text mode
+    if _is_binary_mode(path_or_buf, mode) and "b" not in mode:
+        mode += "b"
 
-    compression, compression_args = get_compression_method(compression)
-    compression = infer_compression(handle, compression)
+    # open URLs
+    ioargs = _get_filepath_or_buffer(
+        path_or_buf,
+        encoding=encoding,
+        compression=compression,
+        mode=mode,
+        storage_options=storage_options,
+    )
+
+    handle = ioargs.filepath_or_buffer
+    handles: List[Buffer]
 
     # memory mapping needs to be the first step
     handle, memory_map, handles = _maybe_memory_map(
-        handle, memory_map, encoding, mode, errors
+        handle, memory_map, ioargs.encoding, ioargs.mode, errors
     )
 
     is_path = isinstance(handle, str)
+    compression_args = dict(ioargs.compression)
+    compression = compression_args.pop("method")
+
     if compression:
+        # compression libraries do not like an explicit text-mode
+        ioargs.mode = ioargs.mode.replace("t", "")
+
         # GZ Compression
         if compression == "gzip":
             if is_path:
                 assert isinstance(handle, str)
-                handle = gzip.GzipFile(filename=handle, mode=mode, **compression_args)
+                handle = gzip.GzipFile(
+                    filename=handle,
+                    mode=ioargs.mode,
+                    **compression_args,
+                )
             else:
                 handle = gzip.GzipFile(
                     fileobj=handle,  # type: ignore[arg-type]
-                    mode=mode,
+                    mode=ioargs.mode,
                     **compression_args,
                 )
 
         # BZ Compression
         elif compression == "bz2":
             handle = bz2.BZ2File(
-                handle, mode=mode, **compression_args  # type: ignore[arg-type]
+                handle,  # type: ignore[arg-type]
+                mode=ioargs.mode,
+                **compression_args,
             )
 
         # ZIP Compression
         elif compression == "zip":
-            handle = _BytesZipFile(handle, mode, **compression_args)
+            handle = _BytesZipFile(handle, ioargs.mode, **compression_args)
             if handle.mode == "r":
                 handles.append(handle)
                 zip_names = handle.namelist()
@@ -658,7 +626,7 @@ def get_handle(
 
         # XZ Compression
         elif compression == "xz":
-            handle = get_lzma_file(lzma)(handle, mode)
+            handle = get_lzma_file(lzma)(handle, ioargs.mode)
 
         # Unrecognized Compression
         else:
@@ -668,42 +636,50 @@ def get_handle(
         assert not isinstance(handle, str)
         handles.append(handle)
 
-    elif is_path:
+    elif isinstance(handle, str):
         # Check whether the filename is to be opened in binary mode.
         # Binary mode does not support 'encoding' and 'newline'.
-        assert isinstance(handle, str)
-        if encoding and "b" not in mode:
+        if ioargs.encoding and "b" not in ioargs.mode:
             # Encoding
-            handle = open(handle, mode, encoding=encoding, errors=errors, newline="")
+            handle = open(
+                handle,
+                ioargs.mode,
+                encoding=ioargs.encoding,
+                errors=errors,
+                newline="",
+            )
         else:
             # Binary mode
-            handle = open(handle, mode)
+            handle = open(handle, ioargs.mode)
         handles.append(handle)
 
     # Convert BytesIO or file objects passed with an encoding
     is_wrapped = False
-    if is_text and (
-        compression
-        or isinstance(handle, need_text_wrapping)
-        or "b" in getattr(handle, "mode", "")
-    ):
+    if is_text and (compression or _is_binary_mode(handle, ioargs.mode)):
         handle = TextIOWrapper(
             handle,  # type: ignore[arg-type]
-            encoding=encoding,
+            encoding=ioargs.encoding,
             errors=errors,
             newline="",
         )
         handles.append(handle)
-        # do not mark as wrapped when the user provided a string
-        is_wrapped = not is_path
+        # only marked as wrapped when the caller provided a handle
+        is_wrapped = not (
+            isinstance(ioargs.filepath_or_buffer, str) or ioargs.should_close
+        )
 
     handles.reverse()  # close the most recently added buffer first
+    if ioargs.should_close:
+        assert not isinstance(ioargs.filepath_or_buffer, str)
+        handles.append(ioargs.filepath_or_buffer)
+
     assert not isinstance(handle, str)
     return IOHandles(
         handle=handle,
         created_handles=handles,
         is_wrapped=is_wrapped,
         is_mmap=memory_map,
+        compression=ioargs.compression,
     )
 
 
@@ -804,7 +780,7 @@ def _maybe_memory_map(
     mode: str,
     errors: Optional[str],
 ) -> Tuple[FileOrBuffer, bool, List[Buffer]]:
-    """Try to use memory map file/buffer."""
+    """Try to memory map file/buffer."""
     handles: List[Buffer] = []
     memory_map &= hasattr(handle, "fileno") or isinstance(handle, str)
     if not memory_map:
@@ -834,3 +810,27 @@ def _maybe_memory_map(
         memory_map = False
 
     return handle, memory_map, handles
+
+
+def file_exists(filepath_or_buffer: FilePathOrBuffer) -> bool:
+    """Test whether file exists."""
+    exists = False
+    filepath_or_buffer = stringify_path(filepath_or_buffer)
+    if not isinstance(filepath_or_buffer, str):
+        return exists
+    try:
+        exists = os.path.exists(filepath_or_buffer)
+        # gh-5874: if the filepath is too long will raise here
+    except (TypeError, ValueError):
+        pass
+    return exists
+
+
+def _is_binary_mode(handle: FilePathOrBuffer, mode: str) -> bool:
+    """Whether the handle is opened in binary mode"""
+    # classes that expect bytes
+    binary_classes = [BufferedIOBase, RawIOBase]
+
+    return isinstance(handle, tuple(binary_classes)) or "b" in getattr(
+        handle, "mode", mode
+    )

commit 0c51920b3aff6fc1c177283700b746abf972b2f1

REGR: be able to read Stata files without reading them fully into memory
Fixes #48700
Refs pandas-dev/pandas#9245
Refs pandas-dev/pandas#37639
Regressed in 6d1541e1782a7b94797d5432922e64a97934cfa4

pandas/io/stata.py
@@ -1164,15 +1164,23 @@ class StataReader(StataParser, abc.Iterator):
         self._lines_read = 0
 
         self._native_byteorder = _set_endianness(sys.byteorder)
-        with get_handle(
+
+        handles = get_handle(
             path_or_buf,
             "rb",
             storage_options=storage_options,
             is_text=False,
             compression=compression,
-        ) as handles:
-            # Copy to BytesIO, and ensure no encoding
-            self.path_or_buf = BytesIO(handles.handle.read())
+        )
+        if hasattr(handles.handle, "seekable") and handles.handle.seekable():
+            # If the handle is directly seekable, use it without an extra copy.
+            self.path_or_buf = handles.handle
+            self._close_file = handles.close
+        else:
+            # Copy to memory, and ensure no encoding.
+            with handles:
+                self.path_or_buf = BytesIO(handles.handle.read())
+            self._close_file = self.path_or_buf.close
 
         self._read_header()
         self._setup_dtype()
@@ -1192,7 +1200,7 @@ class StataReader(StataParser, abc.Iterator):
 
     def close(self) -> None:
         """close the handle if its open"""
-        self.path_or_buf.close()
+        self._close_file()
 
     def _set_encoding(self) -> None:
         """

pandas/tests/io/test_stata.py
@@ -1848,6 +1848,29 @@ def test_backward_compat(version, datapath):
     tm.assert_frame_equal(old_dta, expected, check_dtype=False)
 
 
+def test_direct_read(datapath, monkeypatch):
+    file_path = datapath("io", "data", "stata", "stata-compat-118.dta")
+
+    # Test that opening a file path doesn't buffer the file.
+    with StataReader(file_path) as reader:
+        # Must not have been buffered to memory
+        assert not isinstance(reader.path_or_buf, io.BytesIO)
+        assert not reader.read().empty
+
+    # Test that we use a given fp exactly, if possible.
+    with open(file_path, "rb") as fp:
+        with StataReader(fp) as reader:
+            assert reader.path_or_buf is fp
+            assert not reader.read().empty
+
+    # Test that we use a given BytesIO exactly, if possible.
+    with open(file_path, "rb") as fp:
+        with io.BytesIO(fp.read()) as bio:
+            with StataReader(bio) as reader:
+                assert reader.path_or_buf is bio
+                assert not reader.read().empty
+
+
 @pytest.mark.parametrize("version", [114, 117, 118, 119, None])
 @pytest.mark.parametrize("use_dict", [True, False])
 @pytest.mark.parametrize("infer", [True, False])
@@ -2027,7 +2050,10 @@ def test_stata_compression(compression_only, read_infer, to_infer):
 
     with tm.ensure_clean(filename) as path:
         df.to_stata(path, compression=to_compression)
-        result = read_stata(path, compression=read_compression, index_col="index")
+        with StataReader(
+            path, compression=read_compression, index_col="index"
+        ) as reader:
+            result = reader.read()
         tm.assert_frame_equal(result, df)
 
 